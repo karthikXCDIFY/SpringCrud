463320522829c78693c1b1ee8d59d6983a6bebb8
karthikXCDIFY <karthik.hc@xcdify.com>
2024-02-01 13:30:53 +0530

    initital commite

---
+++ src/main/java/com/example/demo4/repository/SubjectRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo4.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo4.been.Subject;
+
+
+
+public interface SubjectRepository extends CrudRepository<Subject,String> {
+	
+
+}
